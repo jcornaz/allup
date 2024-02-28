4c32f1dad953c6d4e06493438cd7ad63e6fc20f5
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2024-02-28 18:13:29 +0100

    refactor: extract functions in probe module

--- src/probe.rs
+++ src/probe.rs
@@ -1,6 +1,6 @@
 use std::time::{Duration, Instant};
 
-use reqwest::{Client, StatusCode};
+use reqwest::{Client, Response, StatusCode};
 use thiserror::Error;
 
 use crate::config::Endpoint;
@@ -20,26 +20,41 @@ pub enum Error {
     Network(reqwest::Error),
 }
 
-pub async fn probe(endpoint: &Endpoint) -> Result {
-    let req = Client::new()
-        .get(endpoint.url.clone())
-        .timeout(endpoint.timeout);
-
-    let started = Instant::now();
-    let resp = req.send().await;
-    let duration = started.elapsed();
-
-    let resp = resp.map_err(|err| {
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
         if err.is_timeout() {
             Error::Timeout
         } else {
             Error::Network(err)
         }
-    })?;
+    }
+}
+
+pub async fn probe(endpoint: &Endpoint) -> Result {
+    let req = request(endpoint);
+    let (resp, duration) = send(req).await?;
+    ensure_success(resp).await?;
+    Ok(duration)
+}
+
+fn request(endpoint: &Endpoint) -> reqwest::RequestBuilder {
+    Client::new()
+        .get(endpoint.url.clone())
+        .timeout(endpoint.timeout)
+}
+
+async fn send(req: reqwest::RequestBuilder) -> reqwest::Result<(reqwest::Response, Duration)> {
+    let started = Instant::now();
+    let resp = req.send().await?;
+    let duration = started.elapsed();
+    Ok((resp, duration))
+}
+
+async fn ensure_success(resp: Response) -> std::result::Result<(), Error> {
     let status = resp.status();
     if !status.is_success() {
         let body = resp.text().await.ok();
         return Err(Error::UnexpectedResponse { status, body });
     }
-    Ok(duration)
+    Ok(())
 }
